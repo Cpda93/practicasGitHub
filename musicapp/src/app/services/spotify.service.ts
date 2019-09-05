4f24b8412b8c14aaacbea65c34a1df2bcd95e881
Cpda93 <pcjesusantonio@gmail.com>
2019-09-04 23:43:20 -0500

    se agrega operador map de los observables

--- musicapp/src/app/services/spotify.service.ts
+++ musicapp/src/app/services/spotify.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+// Se importa map operador map de los observables
+import { map } from 'rxjs/operators';
+import { pipe } from '@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +15,19 @@ export class SpotifyService {
 
   getNewReleases() {
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQBfQiqufTsK4r5onTsNfOD5jcx2Ix3hteZyoslqq3Wmw-O97FDgOOP7ZUmJBy8KAt_xJRDoFMfbLn9cf0g'
+      Authorization: 'Bearer BQApN9QGkj1Nz3k2nlMJ7aD3Unubaj31onJ6llcq_5Z-BoERr_EZKC4PJPUV8x80erWk9CjVfnW_oK-X9rU'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers});
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers})
+    .pipe( map( data => data[`albums`].items));
   }
+
   getArtista(termino: string) {
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQBfQiqufTsK4r5onTsNfOD5jcx2Ix3hteZyoslqq3Wmw-O97FDgOOP7ZUmJBy8KAt_xJRDoFMfbLn9cf0g'
+      Authorization: 'Bearer BQApN9QGkj1Nz3k2nlMJ7aD3Unubaj31onJ6llcq_5Z-BoERr_EZKC4PJPUV8x80erWk9CjVfnW_oK-X9rU'
     });
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
+    .pipe( map ( data => data[`artists`].items));
   }
 }
