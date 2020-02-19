7545e92007f0075358c311fcdf430ef4c27ce830
Cpda93 <pcjesusantonio@gmail.com>
2020-02-19 17:08:24 -0600

    9.7. Comenzando con Auth0 Componentes y servicios

---
+++ auth/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {APP_ROUTING} from './app.routes';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+// servicios
+import {AuthService} from './services/auth.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING
+  ],
+  providers: [
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
