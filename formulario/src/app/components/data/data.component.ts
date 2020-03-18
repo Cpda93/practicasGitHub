ad4ed3ed013bfbda1e09a01cd740764c5a340ed3
Cpda93 <pcjesusantonio@gmail.com>
2020-03-18 15:52:37 -0600

    10.17. Data Continuando con las validaciones y HTML((se agrgearon clases por medio de ngClass was-validated y  is-invalid))

--- formulario/src/app/components/data/data.component.ts
+++ formulario/src/app/components/data/data.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('',     [ Validators.required,
+                                          Validators.minLength(3)
+                                        ]),
+      'apellido': new FormControl('',   Validators.required),
+      'correo': new FormControl('',  [  Validators.required,
+                                        Validators.pattern("[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}")
+                                      ])
+    })
   }
 
-}
+  guardarCambios(){
+    console.log(this.forma.value);
+    console.log(this.forma)
+  }
+  }
--- formulario/src/app/components/template/template.component.ts
+++ formulario/src/app/components/template/template.component.ts
@@ -14,15 +14,29 @@ export class TemplateComponent {
   usuario:Object = {
     nombre: null,
     apellido: null,
-    correo: null
+    correo: null,
+    pais: "",
+    sexo: "",
+    acepta: false
   }
+  paises = [
+    {
+    codigo: "MX",
+    nombre: "Mexico"
+    },
+    {
+    codigo: "USA",
+    nombre: "Estados Unidos"
+    }
+  ]
+  sexos:string[] = ["Hombre", "Mujer", "Aveces"]
 
   constructor() { }
 
   guardar(forma: any){
-    console.log("Se ha guardado el formulario");
-    console.log(forma);
-    console.log("valor: ", forma.value);
-    console.log("usuario", this.usuario);
+    // console.log("Se ha guardado el formulario");
+    // console.log(forma);
+    // console.log("valor: ", forma.value);
+    console.log("El usuario es:", this.usuario);
   }
 }
